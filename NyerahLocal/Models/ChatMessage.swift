215d2c684e4fdb87555f0f66fb5ad7bdf34b4687
Matthew Talafous <68287092+NyerahMT@users.noreply.github.com>
2026-09-14 00:20:20 -0400

    Add chat message model

---
+++ NyerahLocal/Models/ChatMessage.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct ChatMessage: Identifiable, Equatable {
+    enum Role: Equatable {
+        case user
+        case assistant
+    }
+
+    let id: UUID
+    let role: Role
+    var text: String
+
+    init(id: UUID = UUID(), role: Role, text: String) {
+        self.id = id
+        self.role = role
+        self.text = text
+    }
+}
